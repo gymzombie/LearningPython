7d46d0f0aefe03f1603acfde79954bf51942192b
gymzombie <gymzombie@users.noreply.github.com>
2016-03-15 22:18:48 -0400

    Create for.py

---
+++ for.py
@@ -0,0 +1,14 @@
+#for each in range(0,8,2):
+#    print ("The value of string is "+ str(each))
+    # This is the old-school way of concatenating strings
+
+
+# Newer version is like:
+
+for each in range(0,8,2):
+#    print ("The value of string is %d" %each)
+        # %d means decimal integer, we're typecasting the field
+
+#    print ("The value of string is %s" %str(each))
+
+    print ("The value of string is {0} and its double is {1}".format(each,each*2))
