54d33d36c5eab64e62a52233dbe68fdd02bce6ca
gymzombie <gymzombie@users.noreply.github.com>
2016-03-27 15:22:16 -0400

    Update 6-whileloop.py

--- chapter2-FlowControl/6-whileloop.py
+++ chapter2-FlowControl/6-whileloop.py
@@ -13,9 +13,29 @@ print('Thank you!')
 
 #---------------------
 
+# A break ceases the execution and leaves the loop without rechecking the condition
+
 while True:                         # This is the same as above, but uses a break instead
     print('Please type your name.')
     name = input()                  
     if name == 'your name':         
         break                       
 print('Thank you!')                 
+
+
+#------------------------
+#   Continue Statements:
+# Continue "goes back to the beginning of the of the loop and re-check the condition
+
+while True:
+  print('Who are you?')
+  name = input()
+  if name != 'Joe':       #(1)
+    continue              #(2)
+  print('Hello, Joe. What is the password? (It is a fish.)') 
+  password = input()      #(3)
+  if password == 'swordfish':
+    break                 #(4)
+print('Access granted.')  #(5)
+
+# ------------------------
