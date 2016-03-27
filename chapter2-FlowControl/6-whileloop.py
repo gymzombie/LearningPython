4bbc0b023fd2144c59fe3d9b6ac9d819f90d6abc
gymzombie <gymzombie@users.noreply.github.com>
2016-03-27 15:19:18 -0400

    Update 6-whileloop.py

--- chapter2-FlowControl/6-whileloop.py
+++ chapter2-FlowControl/6-whileloop.py
@@ -1,1 +1,21 @@
+spam = 0
+while spam <5:
+  print('Hello World!')
+  spam = spam + 1
+  
+#--------------------
 
+name = ''                           # Programmer humor
+while name != 'your name':          
+    print('Please type your name.')
+    name = input()                  
+print('Thank you!')                 
+
+#---------------------
+
+while True:                         # This is the same as above, but uses a break instead
+    print('Please type your name.')
+    name = input()                  
+    if name == 'your name':         
+        break                       
+print('Thank you!')                 
