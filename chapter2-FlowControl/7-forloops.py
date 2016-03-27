12bec31d4edc2599194c5c1ad7ac3d1f346c7b47
gymzombie <gymzombie@users.noreply.github.com>
2016-03-27 15:33:15 -0400

    Update 7-forloops.py

--- chapter2-FlowControl/7-forloops.py
+++ chapter2-FlowControl/7-forloops.py
@@ -3,6 +3,21 @@ print('My name is')
 for i in range(5):
   print('Jimmy Five Times ' + str(i))
   
+ # Same as above, just in a different format
+ 
+print('My name is')
+i  = 0
+while i < 5
+  print('Jimmy Five Times ' + str(i))
+  i = i + 1  
+  
+  # Other options to range()
+print('My name is')
+for i in range(0, 10, 2):
+  print('Jimmy Five Times ' + str(i))
+  
+
+
   
   #----------------------
 
