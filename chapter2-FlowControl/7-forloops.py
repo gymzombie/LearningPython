248e9b14fc5a3262833a9c44746baa07df48effb
gymzombie <gymzombie@users.noreply.github.com>
2016-03-27 15:27:19 -0400

    Create 7-forloops.py

---
+++ chapter2-FlowControl/7-forloops.py
@@ -0,0 +1,15 @@
+
+print('My name is')
+for i in range(5):
+  print('Jimmy Five Times ' + str(i))
+  
+  
+  #----------------------
+
+# This one calculates adding all number from 0-100  
+total = 0
+for num in range(101):
+	total = total + num
+print(total)
+
+
