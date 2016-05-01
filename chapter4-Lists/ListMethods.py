bd3b31fbb3da300e51427d120b8047a68459fb9f
gymzombie <gymzombie@users.noreply.github.com>
2016-05-01 14:38:38 -0400

    Create ListMethods.py

---
+++ chapter4-Lists/ListMethods.py
@@ -0,0 +1,14 @@
+
+# A method is a function that is called on a particular value
+
+
+>>> spam = ['hello', 'hi', 'howdy', 'heyas']
+>>> spam.index('hello')
+0
+
+1. You can't call a method by itself. AKA index('hello') doesn't do anything because it doesn't reference spam
+ 
+  
+Methods for lists:
+   Index figures out where in the list the value is stored. i.e. spam.index('hi') will return 1. 
+   
